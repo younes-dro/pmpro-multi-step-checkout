fc05690026acafa0f902cc84d2a0b38211682863
Younes DRO <dro66_8@hotmail.fr>
2024-04-11 17:01:08 +0000

    Create class-multi-step-checkout-pmpro-settings.php

---
+++ includes/class-multi-step-checkout-pmpro-settings.php
@@ -0,0 +1,13 @@
+<?php
+
+if ( ! class_exists( 'Multi_Step_Checkout_Pmpro_Settings' ) ) {
+
+	class Multi_Step_Checkout_Pmpro_Settings {
+
+		public function __consctrunct() {
+
+			// add_action( 'admin_menu', array ( $this , 'multi_step_checkout' ) );
+		}
+	}
+}
+
